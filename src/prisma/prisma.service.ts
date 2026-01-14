ff3e0c6eea8159db403f0d0927e697518ec7f4af
aldairl <aldairlunabuitron@gmail.com>
2026-01-14 06:38:09 -0500

    feat: on init and on destroy method

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,9 +3,17 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg'
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     constructor() {
         const adapter = new PrismaPg({ url: process.env.DATABASE_URL});
         super({adapter})
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
     }
 }
